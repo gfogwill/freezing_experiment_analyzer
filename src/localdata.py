c6418f8c0e3d6b498db5c7b6dd6a97c9645da766
perezfo <german.perez-fogwill@fmi.fi>
2022-08-30 10:00:05 +0300

    refactor: load images from pics dir

--- src/localdata.py
+++ src/localdata.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 
 import numpy as np
@@ -7,9 +8,20 @@ from src import paths
 
 def load_images_file_list(experiment_name):
     # Load all the images
-    files_path = pathlib.Path(paths.raw_data_path / experiment_name)
-    files_list = sorted(pathlib.Path(files_path).glob('*.jpg'), key=lambda path: int(path.stem))
-    return files_list
+    experiment_path = pathlib.Path(paths.raw_data_path / experiment_name)
+    pics_path = experiment_path / 'pics'
+
+    if not experiment_path.exists():
+        logging.error(f"Experiment {experiment_name} does not exist!")
+        raise FileNotFoundError
+
+    pics_list = sorted(pics_path.glob('*.jpg'), key=lambda path: int(path.stem))
+
+    if pics_list.__len__() == 0:
+        logging.error("No images found!")
+        raise FileNotFoundError
+
+    return pics_list
 
 
 def load_log_data(experiment_name):
