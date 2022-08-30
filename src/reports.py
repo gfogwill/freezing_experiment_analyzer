0e01bf52662686573b6edc27d97cc1e573bc96e1
perezfo <german.perez-fogwill@fmi.fi>
2022-08-30 12:32:56 +0300

    feat: create video of the experiment

--- src/reports.py
+++ src/reports.py
@@ -7,11 +7,12 @@ import cv2
 from src.localdata import load_log_data
 
 
-def generate_reports(experiment_name, pics_list, freezing_idxs, out_path):
+def generate_reports(experiment_name, pics_list, circles_positions, freezing_idxs, out_path, crop_values):
 
     click.echo(f"Generating reports in path: {out_path}")
 
     pathlib.Path(out_path).mkdir(parents=True, exist_ok=True)
+
     data = load_log_data(experiment_name)
 
     t = []
@@ -23,21 +24,32 @@ def generate_reports(experiment_name, pics_list, freezing_idxs, out_path):
             ff.append((np.array(freezing_idxs) <= i).sum()/freezing_idxs.__len__())
             fo.write(f'{i},{t[-1]},{ff[-1]}\n')
 
-    generate_video(pics_list, out_path)
+    generate_video(pics_list, circles_positions, freezing_idxs, out_path, crop_values)
 
     click.echo("Reports done!")
 
 
-def generate_video(pic_list, out_path):
+def generate_video(pic_list, circles_positions, freezing_idxs, out_path, crop_values=None):
+    circles = np.uint16(np.around(circles_positions))
+
     img_array = []
     for filename in pic_list:
-        img = cv2.imread(filename)
+        img = cv2.imread(str(filename))
+        if crop_values is not None:
+            img = img[crop_values[1]:crop_values[3], crop_values[0]:crop_values[2]]
         height, width, layers = img.shape
         size = (width, height)
         img_array.append(img)
 
-    out = cv2.VideoWriter(out_path / 'video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+        for n, i in enumerate(circles):
+            if int(filename.stem) < freezing_idxs[n]:
+                cv2.circle(img, (i[0], i[1]), i[2], (0, 0, 255), 1)
+            else:
+                cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 1)
+
+    out = cv2.VideoWriter(str(out_path / 'video.avi'), cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
 
     for i in range(len(img_array)):
         out.write(img_array[i])
+
     out.release()
