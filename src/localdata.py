ee8f098312946325b9121c69d0bee9ec203c2484
perezfo <german.perez-fogwill@fmi.fi>
2022-10-11 12:14:09 +0200

    change local_data (DRINCZ)

--- src/localdata.py
+++ src/localdata.py
@@ -2,6 +2,7 @@ import logging
 import pathlib
 
 import numpy as np
+import datetime as dt
 
 from src import paths
 
@@ -15,7 +16,7 @@ def load_images_file_list(experiment_name):
         logging.error(f"Experiment {experiment_name} does not exist!\n Check dir: {experiment_path}")
         exit()
 
-    pics_list = sorted(pics_path.glob('*.png'), key=lambda path: int(path.stem))
+    pics_list = sorted(pics_path.glob('*.jpg'), key=lambda path: int(path.stem))
 
     if pics_list.__len__() == 0:
         logging.error("No images found!")
@@ -24,8 +25,23 @@ def load_images_file_list(experiment_name):
     return pics_list
 
 
+def date_parser(s):
+    return np.datetime64(dt.datetime.strptime(s, '%d/%m/%Y %H:%M:%S'))
+
+
 def load_log_data(experiment_name):
-    return np.genfromtxt(paths.raw_data_path / experiment_name / 'log_data.csv', delimiter=',', names=True)
+    # return np.genfromtxt(paths.raw_data_path / experiment_name / 'ramp.txt', delimiter=' \t ', names=True)
+    return np.genfromtxt(paths.raw_data_path / experiment_name / 'ramp.txt',
+                         delimiter=' \t ',
+                         encoding='iso-8859-1',
+                         dtype=[('julian', '<f8'),
+                                ('date', 'datetime64[s]'),
+                                ('time', '<f8'),
+                                ('Ramp_ON', 'bool'),
+                                ('RampSetTemp_°C', '<f8'),
+                                ('Bath_Temp_°C', '<f8')],
+                         skip_header=1,
+                         converters={1: date_parser})
 
 
 def load_readme(experiment_name):
